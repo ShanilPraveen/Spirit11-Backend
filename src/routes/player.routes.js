c91d91bbe39e2f70a176819a91f30c43b676cee0
ShanilPraveen <jspraveen2002@gmail.com>
2025-07-17 21:44:03 +0530

    player routes

---
+++ src/routes/player.routes.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = express.Router();
+const {PrismaClient} = require('@prisma/client');
+const prisma  = new PrismaClient();
+
+router.get("/",async(req,res)=>{
+    try{
+        const players = await prisma.player.findMany({
+            orderBy: {
+                name: 'asc'}
+        });
+        res.status(200).json(players);
+    }
+    catch(error){
+        console.error("Error fetching players:", error);
+        res.status(500).json({error: "Internal Server Error"});
+    }
+
+})
+
+router.get("/:id",async(req,res)=>{
+    try{
+        const player = await prisma.player.findUnique({
+            where: {id: req.params.id}
+        });
+        if(!player){
+            return res.status(404).json({error: "Player not found"});
+        }
+        res.status(200).json(player);
+        //console.log(player);
+    }
+    catch(error){
+        console.error("Error fetching player:", error);
+        res.status(500).json({error: "Internal Server Error"});
+    }
+})
+
+module.exports = router;
