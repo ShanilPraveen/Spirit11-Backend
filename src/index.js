24e39a4786c88df917bc683b5cece2670205b557
shanilpraveen <jspraveen2002@gmail.com>
2026-05-30 00:11:23 +0530

    Refresh token passing through cookies

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
-const cors = require('cors')
 require('dotenv').config();
 const playerRoutes = require('./routes/player.routes');
 const teamRoutes = require('./routes/team.routes');
 const authRoutes = require('./routes/auth.routes');
 const leaderboardRoutes = require('./routes/leaderboard.routes');
-
+const cookieParser = require('cookie-parser');
 const app = express();
 
-app.use(cors());
+const cors = require('cors');
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use('/api/players', playerRoutes);
 app.use('/api/teams', teamRoutes);
 app.use('/api/auth', authRoutes);
